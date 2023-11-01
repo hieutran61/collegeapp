00d937728813722ca840d1e2c6478b3fe54b63dd
TonyToan <toantdkde160402@fpt.edu.vn>
2023-11-01 11:23:40 +0700

    video 16 layout Delete Notice

---
+++ app/src/main/java/com/fpt/collegeapp/notice/DeleteNoticeActivity.java
@@ -0,0 +1,16 @@
+package com.fpt.collegeapp.notice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fpt.collegeapp.R;
+
+public class DeleteNoticeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_noticectivity);
+    }
+}
