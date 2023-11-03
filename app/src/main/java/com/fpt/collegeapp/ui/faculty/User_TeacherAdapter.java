52fe372375476fb293adaebc5c0f4f0f1ba07c4d
TonyToan <toantdkde160402@fpt.edu.vn>
2023-11-03 12:45:11 +0700

    done 26

---
+++ app/src/main/java/com/fpt/collegeapp/ui/faculty/User_TeacherAdapter.java
@@ -0,0 +1,70 @@
+package com.fpt.collegeapp.ui.faculty;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.fpt.collegeapp.R;
+
+import java.util.List;
+
+public class User_TeacherAdapter extends RecyclerView.Adapter<User_TeacherAdapter.TeacherViewAdapter> {
+
+    private List<User_TeacherData> list;
+    private Context context;
+
+
+    public User_TeacherAdapter(List<User_TeacherData> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public TeacherViewAdapter onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.faculty_item_layout, parent, false);
+        return new TeacherViewAdapter(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TeacherViewAdapter holder, int position) {
+
+        User_TeacherData item = list.get(position);
+        holder.name.setText(item.getName());
+        holder.email.setText(item.getEmail());
+        holder.post.setText(item.getPost());
+        try {
+            Glide.with(holder.itemView.getContext())  // Use the context from the item view
+                    .load(item.getImage()).placeholder(R.drawable.avatar_profile) // Replace with the actual image URL or resource
+                    .into(holder.imageView);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class TeacherViewAdapter extends RecyclerView.ViewHolder {
+
+        private TextView name, email, post;
+        private ImageView imageView;
+
+        public TeacherViewAdapter(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.teacherName);
+            email = itemView.findViewById(R.id.teacherEmail);
+            post = itemView.findViewById(R.id.teacherPost);
+            imageView = itemView.findViewById(R.id.teacherImage);
+        }
+    }
+}
